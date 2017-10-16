1e127d6a3643399dfe5b3a157140a3c8b0a17255
Nurbek <nurbek@inbox.ru>
2017-10-16 17:16:48 +0400

    Fixed pep8, remake to list comp, added checking filepath with argparse module,fixed iteration and readme

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # The nearest bars
 
-The script helps to find biggest, smallest and closest bar
+The script helps to find biggests, smallests and closest bar(s)
 
 # Quickstart
+
+Before start to look for the nearest bars you may to download bars.json file from this repository to script folder
 Launch on Linux(Python 3.5) or Windows as simple
 
-```#!bash
-
-$ python bars.py <path_to_file>
-# For example, python bars.py '/home/user/downloads/bars.json'
+```bash
+# For example, python bars.py bars.json
+# You will see:
+ Самые большие бары:  ['Спорт бар «Красная машина»']
+ Самые маленькие бары:  ['БАР. СОКИ', 'Соки', 'Фреш-бар', 'Бар в Деловом центре Яуза']
+# Then you need to input coordinates, for example:
+ Enter longitude: 54
+ Enter latitude: 55
+ Самый близкий бар:  Бар «Гости»
 ```
 
 # Project Goals
--- bars.py
+++ bars.py
@@ -1,39 +1,36 @@
+# -*- coding: utf-8 -*-
+import argparse
 import json
-import os
 import sys
 from collections import Counter
 from haversine import haversine
-from math import radians, cos, sin, asin, sqrt
 
 
 def load_data(filepath):
-    if not os.path.exists(filepath):
-        return None
     with open(filepath, 'r') as raw_json_file:
         return json.load(raw_json_file)
 
 
-def get_biggest_bar(json_data):
-    seatscounts = []
-    target_bars = []
-    for i in json_data['features']:
-        seatscounts.append(i['properties']['Attributes']['SeatsCount'])
-    for i in range(len(json_data['features'])):
-        if json_data['features'][i]['properties']['Attributes']['SeatsCount'] == \
-           max(seatscounts):
-            target_bars.append(data['features'][i]['properties']['Attributes']['Name'])
+def get_biggest_bars(json_data):
+    seatscounts = [i['properties']['Attributes']['SeatsCount']
+                   for i in json_data['features']]
+
+    target_bars = [i['properties']['Attributes']['Name']
+                   for i in json_data['features']
+                   if i['properties']['Attributes']['SeatsCount'] ==
+                   max(seatscounts)
+                   ]
     return target_bars
 
 
-def get_smallest_bar(json_data):
-    seatscounts = []
-    target_bars = []
-    for i in json_data['features']:
-        seatscounts.append(i['properties']['Attributes']['SeatsCount'])
-    for i in range(len(json_data['features'])):
-        if json_data['features'][i]['properties']['Attributes']['SeatsCount'] == \
-           min(seatscounts):
-            target_bars.append(data['features'][i]['properties']['Attributes']['Name'])
+def get_smallest_bars(json_data):
+    seatscounts = [i['properties']['Attributes']['SeatsCount']
+                   for i in json_data['features']]
+    target_bars = [i['properties']['Attributes']['Name']
+                   for i in json_data['features']
+                   if i['properties']['Attributes']['SeatsCount'] ==
+                   min(seatscounts)
+                   ]
     return target_bars
 
 
@@ -48,18 +45,23 @@ def min_unique(dictionary):
 
 def get_closest_bar(json_data, longitude, latitude):
     current = (latitude, longitude)
-    coordinates = {json_data['features'][i]['properties']['Attributes']['Name']: \
-                  tuple(list(reversed(json_data['features'][i]['geometry']['coordinates']))) for i in range(len(json_data['features']))}
-    haversine_pool = {key: haversine(coordinates[key], current) for key in coordinates}
+    coordinates = {i['properties']['Attributes']['Name']:
+                   i['geometry']['coordinates'][::-1]
+                   for i in json_data['features']}
+    haversine_pool = {key: haversine(coordinates[key], current)
+                      for key in coordinates}
     return min_unique(haversine_pool)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        filepath = sys.argv[1]
-        source_json = load_data(filepath)
-        print('Самые большие бары: ', get_biggest_bar(source_json))
-        print('Самые маленькие бары: ', get_smallest_bar(source_json))
-        print('Самый близкий бар: ', get_closest_bar(source_json, float(input('Enter longitude: ')), float(input('Enter latitude: '))))
-    else:
-        print("Error! Enter path!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='Path to file')
+    args = parser.parse_args()
+    filepath = args.filepath    
+    source_json = load_data(filepath)
+    print('Самые большие бары: ', get_biggest_bars(source_json))
+    print('Самые маленькие бары: ', get_smallest_bars(source_json))
+    print('Самый близкий бар: ',
+          get_closest_bar(source_json,
+                          float(input('Enter longitude: ')),
+                          float(input('Enter latitude: '))))
