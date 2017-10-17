0f02f04698423d7deeab84e10b187cc0739ac185
Nurbek <nurbek@inbox.ru>
2017-10-17 16:22:24 +0400

    Fixed code with pep8

--- bars.py
+++ bars.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-from collections import Counter
 from haversine import haversine
 
 
@@ -10,15 +9,13 @@ def load_data(filepath):
 
 
 def get_biggest_bar(json_data):
-    return max(json_data, key=lambda bar: bar['properties']
-                                               ['Attributes']
-                                               ['SeatsCount'])
+    return max(json_data,
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(json_data):
-    return min(json_data, key=lambda bar: bar['properties']
-                                               ['Attributes']
-                                               ['SeatsCount'])
+    return min(json_data,
+               key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(json_data, longitude, latitude):
@@ -28,24 +25,22 @@ def get_closest_bar(json_data, longitude, latitude):
                    for bar in json_data}
     haversine_pool = {key: haversine(coordinates[key], current)
                       for key in coordinates}
-    return min(haversine_pool, key = lambda key:haversine_pool[key])
+    return min(haversine_pool, key=lambda key: haversine_pool[key])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filepath', help='Path to file')
     args = parser.parse_args()
-    filepath = args.filepath    
+    filepath = args.filepath
     bars_list = load_data(filepath)['features']
     biggest_bar = get_biggest_bar(bars_list)
     smallest_bar = get_smallest_bar(bars_list)
     closest_bar = get_closest_bar(bars_list,
-                                   float(input('Enter longitude: ')),
-                                   float(input('Enter latitude: ')))
-    print('The biggest bar: ', biggest_bar["properties"]
-                                              ["Attributes"]
-                                              ["Name"])
-    print('The samallest bar: ', smallest_bar["properties"]
-                                             ["Attributes"]
-                                             ["Name"])
+                                  float(input('Enter longitude: ')),
+                                  float(input('Enter latitude: ')))
+    print('The biggest bar: ',
+          biggest_bar["properties"]["Attributes"]["Name"])
+    print('The samallest bar: ',
+          smallest_bar["properties"]["Attributes"]["Name"])
     print('The closest bar: ', closest_bar)
